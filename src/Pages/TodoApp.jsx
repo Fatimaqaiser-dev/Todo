aaa6340ce5517326a4f76da9560f4af921c6d09e
Fatima Qaiser <chohanfatima1231@gmail.com>
2026-06-28 08:15:58 -0700

    Fix Components import path

--- src/Pages/TodoApp.jsx
+++ src/Pages/TodoApp.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import TodoInput from "../components/TodoInput";
-import TodoList from "../components/TodoList";
+import TodoInput from "../Components/TodoInput";
+import TodoList from "../Components/TodoList";
 
 const TodoApp = () => {
   const [task, setTask] = useState("");
